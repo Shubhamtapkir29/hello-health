369f248f8509e29c4961812c794dd5101a0e551d
shubham tapkir <shubhamtapkir@shubhams-MacBook-Air.local>
2025-08-19 11:32:25 -0700

    4th commit

--- hello-health/src/main/java/com/hellohealth/controller/LoginServlet.java
+++ hello-health/src/main/java/com/hellohealth/controller/LoginServlet.java
@@ -1,31 +1,36 @@
-package controller;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import java.io.IOException;
-import dao.UserDAO;
-import model.User;
+package com.hellohealth.controller;
 
 import com.hellohealth.dao.UserDAO;
 import com.hellohealth.model.User;
-
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
 
 public class LoginServlet extends HttpServlet {
     private UserDAO userDAO = new UserDAO();
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
         User user = userDAO.validateUser(email, password);
-
         if (user != null) {
             HttpSession session = request.getSession();
             session.setAttribute("user", user);
-            response.sendRedirect("dashboard.jsp");
+
+            if ("doctor".equalsIgnoreCase(user.getRole())) {
+                response.sendRedirect("doctorDashboard.jsp");
+            } else if ("patient".equalsIgnoreCase(user.getRole())) {
+                response.sendRedirect("patientDashboard.jsp");
+            } else if ("admin".equalsIgnoreCase(user.getRole())) {
+                response.sendRedirect("adminDashboard.jsp");
+            } else {
+                response.sendRedirect("login.jsp?error=invalid");
+            }
         } else {
-            response.sendRedirect("login.jsp?error=1");
+            response.sendRedirect("login.jsp?error=invalid");
         }
     }
 }
