1916b8f953b3b6ed2451d3aea4a92fa8591b3099
Shubhamtapkir29 <138622204+Shubhamtapkir29@users.noreply.github.com>
2025-08-29 00:45:18 -0700

    Update RegisterServlet.java

--- src/main/java/com/hellohealth/controller/RegisterServlet.java
+++ src/main/java/com/hellohealth/controller/RegisterServlet.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com.hellohealth.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -6,25 +6,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.hellohealth.dao.UserDAO;
+import com.hellohealth.model.User;
+import com.hellohealth.util.EmailUtil;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private UserDAO userDAO;
+
+    @Override
+    public void init() throws ServletException {
+        userDAO = new UserDAO();
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String name = request.getParameter("name");
-        String email = request.getParameter("email");
+        String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String email = request.getParameter("email");
         String role = request.getParameter("role");
 
-        // For now just print (later we can save in DB)
-        response.setContentType("text/html");
-        response.getWriter().println("<h2>Registration Successful!</h2>");
-        response.getWriter().println("<p>Name: " + name + "</p>");
-        response.getWriter().println("<p>Email: " + email + "</p>");
-        response.getWriter().println("<p>Role: " + role + "</p>");
+        User newUser = new User(username, password, email, role);
+
+        boolean userAdded = userDAO.addUser(newUser);
+
+        if (userAdded) {
+            // Send confirmation email
+            String subject = "Welcome to HelloHealth";
+            String message = "Dear " + username + ",\n\nThank you for registering on HelloHealth!";
+            EmailUtil.sendEmail(email, subject, message);
+
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+            response.sendRedirect("login.jsp");
+        } else {
+            response.sendRedirect("register.jsp?error=1");
+        }
     }
 }
