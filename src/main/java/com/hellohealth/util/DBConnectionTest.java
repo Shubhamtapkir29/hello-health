1fa48e7938aa910f99e6b6c72d30473b790da713
Shubhamtapkir29 <138622204+Shubhamtapkir29@users.noreply.github.com>
2025-08-30 06:46:28 -0700

    Update DBConnectionTest.java

--- src/main/java/com/hellohealth/util/DBConnectionTest.java
+++ src/main/java/com/hellohealth/util/DBConnectionTest.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class DBConnectionTest {
     public static void main(String[] args) {
-        String jdbcURL = "jdbc:mysql://hello_health.cn42u8e648n1.ap-south-1.rds.amazonaws.com:3306/hello_health";
+        String jdbcURL = "jdbc:mysql://hello-health.cn42u8e648n1.ap-south-1.rds.amazonaws.com:3306/hello_health";
         String dbUser = "admin";   // change if needed
-        String dbPassword = "yourpassword";  // use same as RDS
+        String dbPassword = "admin123";  // use same as RDS
 
         try {
             // Load MySQL driver (needed for Tomcat 9 / Java 8+)
